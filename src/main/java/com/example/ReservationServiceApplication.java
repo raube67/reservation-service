0997779dc36ea13b9bad3eb602a7df32f45ce9b1
raube67 <raube67@t-online.de>
2016-10-16 16:41:13 +0200

    Using Config Service

--- src/main/java/com/example/ReservationServiceApplication.java
+++ src/main/java/com/example/ReservationServiceApplication.java
@@ -3,6 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Bootiful Microservice (Josh Long)
+ * https://www.youtube.com/watch?v=rqQOSG0DWPY
+ * 
+ * @author Agnes
+ *
+ */
 @SpringBootApplication
 public class ReservationServiceApplication {
 
